e1773d89c25816a6c83d5044c44547a7736859bb
lizhaoyu11 <2770404066@qq.com>
2020-09-12 16:21:09 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
+  </div>
+</template>
+
+<script>
+import MainTabBar from './components/MainTabBar'
+export default {
+  name: 'App',
+  components: {
+    MainTabBar
+  }
+}
+
+</script>
+
+<style>
+  /* @import "./assets/css/base.css/"; */
+  #app{
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/components/MainTabBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <tab-bar>
+        <tab-bar-item path='/home' >
+          <img slot="item-icon" src="../assets/img/home.png" alt="">
+          <img slot="item-icon-active" src="../assets/img/home_active.png" alt="">  
+          <div slot="item-text">首页</div>
+        </tab-bar-item >
+
+        <tab-bar-item path='/category'>
+          <img slot="item-icon" src="../assets/img/category.png" alt=""> 
+          <img slot="item-icon-active" src="../assets/img/category_active.png" alt=""> 
+          <div slot="item-text">分类</div>
+        </tab-bar-item>
+
+        <tab-bar-item path='/shopcart'>
+          <img slot="item-icon" src="../assets/img/shopcart.png" alt=""> 
+          <img slot="item-icon-active" src="../assets/img/shopcart_active.png" alt=""> 
+          <div slot="item-text">购物车</div>
+        </tab-bar-item>
+
+        <tab-bar-item path='/profile'>
+          <img slot="item-icon" src="../assets/img/profile.png" alt=""> 
+          <img slot="item-icon-active" src="../assets/img/profile_active.png" alt=""> 
+          <div slot="item-text">我的</div>
+        </tab-bar-item>
+      </tab-bar>
+</template>
+
+<script>
+import TabBar from './tabbar/Tabbar'
+import TabBarItem from './tabbar/TabBarItem'
+export default {
+    name:'MainTabBar',
+    components:{
+        TabBar,
+        TabBarItem
+    }
+}
+</script>
+
+<style>
+
+</style>
